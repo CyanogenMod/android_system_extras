22fdda41c341ba23bb1ebc4a97ca527907a31e4a
Steve Kondik <shade@chemlab.org>
2010-01-04 10:18:05 -0500

    Disable build of normal su binary since we use Zinx's version.

--- su/Android.mk
+++ su/Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(BUILD_OLD_SU),true)
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -13,3 +15,5 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 LOCAL_MODULE_TAGS := debug
 
 include $(BUILD_EXECUTABLE)
+
+endif
