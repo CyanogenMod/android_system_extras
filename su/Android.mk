0b5faf8337f75a76a03685d25c4d961af0eaf6e3
ChainsDD <chainsdd@gmail.com>
2010-08-15 21:38:07 -0500

    su: Replace old su with su for Superuser.apk
    Change-Id: I39b04b00449d7f9ace78b203696737612d0f8fba

--- su/Android.mk
+++ su/Android.mk
@@ -1,15 +1,18 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES:= su.c
+LOCAL_MODULE := su
+LOCAL_SRC_FILES := su.c activity.cpp
 
-LOCAL_MODULE:= su
+LOCAL_C_INCLUDES += external/sqlite/dist
 
-LOCAL_FORCE_STATIC_EXECUTABLE := true
-
-LOCAL_STATIC_LIBRARIES := libc
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libsqlite \
+    libcutils \
+    libbinder \
+    libutils
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
-LOCAL_MODULE_TAGS := debug
 
 include $(BUILD_EXECUTABLE)
