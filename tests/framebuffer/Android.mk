6f23f0d48fc29e645c4afafd1e3b6445887d7328
Jeff Brown <jeffbrown@google.com>
2011-07-11 22:12:33 -0700

    Remove the simulator target from all makefiles. Bug: 5010576
    Change-Id: I5a7df94d99dcb6f1de98bd9b8f659fd51a491c82

--- tests/framebuffer/Android.mk
+++ tests/framebuffer/Android.mk
@@ -11,18 +11,10 @@ LOCAL_MODULE:= test-fb-refresh
 
 LOCAL_MODULE_TAGS := optional
 
-ifeq ($(TARGET_SIMULATOR),true)
-  ifeq ($(HOST_OS),linux)
-    # need this for clock_gettime()
-    LOCAL_LDLIBS += -lrt
-  endif
-endif
-
 include $(BUILD_EXECUTABLE)
 
 ##
 
-ifneq ($(TARGET_SIMULATOR),true)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := fb_test.c
 LOCAL_MODULE = test-fb-simple
@@ -38,5 +30,3 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES := libc
 include $(BUILD_EXECUTABLE)
-
-endif # sim
