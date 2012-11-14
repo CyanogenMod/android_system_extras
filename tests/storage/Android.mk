6002c64f41c3e169e68eeb4c5272c6ec63642514
Ken Sumrall <ksumrall@android.com>
2012-11-13 16:26:59 -0800

    A testing/debugging tool to wipe partitions
    This is basically a command line tool version of the wipe_block_device()
    function in libext4_utils.  Useful for testing and/or debugging the
    BLKSECDISCARD and BLKDISCARD ioctls.
    
    Change-Id: I733f0f7daa673624ddbaac055d82b64d741acd70

--- tests/storage/Android.mk
+++ tests/storage/Android.mk
@@ -9,3 +9,9 @@ LOCAL_MODULE := opentest
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_EXECUTABLE)
 
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES   := wipe_blkdev.c
+LOCAL_MODULE      := wipe_blkdev
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
+
