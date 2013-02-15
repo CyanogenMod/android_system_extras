82ac1af86d8ca3b7a42af9ca02e4d0b556681bc8
Christopher Ferris <cferris@google.com>
2013-02-15 12:27:58 -0800

    Update micro_bench.
    Moving the code to cpp to access the cpuset CPU* macros (these
    macros are defined in sched.h inside of __USE_GNU which is not
    defined for the thumb C compiler). The C++ code is also slightly
    easier to read.
    Add code to set the priority of the process to the highest value.
    Add code to lock the process to a single cpu.
    Add the ability to compute average and standard deviation over
    a number of iterations.
    Change the timing code to use nanosecond resolution timing.
    Add options to allow modification of the alignment of the src/dst
    pointers for memcpy and the dst pointer for memset.
    Add an option to change the size of the data being copied in each
    iteration.
    
    Change-Id: Ib7c50ed4463f94e638eb81690fe8fe0d0bc3ea80

--- micro_bench/Android.mk
+++ micro_bench/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := micro_bench.c
+LOCAL_SRC_FILES := micro_bench.cpp
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 LOCAL_MODULE_TAGS := debug
