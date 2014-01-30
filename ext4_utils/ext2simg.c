jbeich/platform_system_extras

ext4_utils: fix build for 64 bit
Correct u64/s64 definitions
Use PRIu64 to print u64

Change-Id: Icaf7998946b0a2faa03495b404af8b94e4e3d047

ext4_utils/ext2simg.c
@@ -22,6 +22,7 @@
 #include <sys/types.h>
 #include <sys/mman.h>
 #include <fcntl.h>
+#include <inttypes.h>
 #include <libgen.h>
 #include <unistd.h>
 
@@ -86,13 +87,13 @@ static int read_ext(int fd)
 
 	if (verbose) {
 		printf("Found filesystem with parameters:\n");
-		printf("    Size: %llu\n", info.len);
+		printf("    Size: %" PRIu64 "\n", info.len);
 		printf("    Block size: %d\n", info.block_size);
 		printf("    Blocks per group: %d\n", info.blocks_per_group);
 		printf("    Inodes per group: %d\n", info.inodes_per_group);
 		printf("    Inode size: %d\n", info.inode_size);
 		printf("    Label: %s\n", info.label);
-		printf("    Blocks: %llu\n", aux_info.len_blocks);
+		printf("    Blocks: %" PRIu64 "\n", aux_info.len_blocks);
 		printf("    Block groups: %d\n", aux_info.groups);
 		printf("    Reserved block group size: %d\n", info.bg_desc_reserve_blocks);
 		printf("    Used %d/%d inodes and %d/%d blocks\n",

ext4_utils/ext4fixup.c
@@ -25,6 +25,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <inttypes.h>
 #include <unistd.h>
 
 #ifndef USE_MINGW
@@ -235,13 +236,13 @@ static int read_ext(int fd)
 
     if (verbose) {
         printf("Found filesystem with parameters:\n");
-        printf("    Size: %llu\n", info.len);
+        printf("    Size: %" PRIu64 "\n", info.len);
         printf("    Block size: %d\n", info.block_size);
         printf("    Blocks per group: %d\n", info.blocks_per_group);
         printf("    Inodes per group: %d\n", info.inodes_per_group);
         printf("    Inode size: %d\n", info.inode_size);
         printf("    Label: %s\n", info.label);
-        printf("    Blocks: %llu\n", aux_info.len_blocks);
+        printf("    Blocks: %" PRIu64 "\n", aux_info.len_blocks);
         printf("    Block groups: %d\n", aux_info.groups);
         printf("    Reserved block group size: %d\n", info.bg_desc_reserve_blocks);
         printf("    Used %d/%d inodes and %d/%d blocks\n",

ext4_utils/make_ext4fs.c
@@ -26,6 +26,7 @@
 #include <assert.h>
 #include <dirent.h>
 #include <fcntl.h>
+#include <inttypes.h>
 #include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -531,7 +532,7 @@ int make_ext4fs_internal(int fd, const char *_directory,
 	info.bg_desc_reserve_blocks = compute_bg_desc_reserve_blocks();
 
 	printf("Creating filesystem with parameters:\n");
-	printf("    Size: %llu\n", info.len);
+	printf("    Size: %" PRIu64 "\n", info.len);
 	printf("    Block size: %d\n", info.block_size);
 	printf("    Blocks per group: %d\n", info.blocks_per_group);
 	printf("    Inodes per group: %d\n", info.inodes_per_group);
@@ -541,7 +542,7 @@ int make_ext4fs_internal(int fd, const char *_directory,
 
 	ext4_create_fs_aux_info();
 
-	printf("    Blocks: %llu\n", aux_info.len_blocks);
+	printf("    Blocks: %" PRIu64 "\n", aux_info.len_blocks);
 	printf("    Block groups: %d\n", aux_info.groups);
 	printf("    Reserved block group size: %d\n", info.bg_desc_reserve_blocks);
 

CyanogenMod/android_system_extras

ext4_utils/ext2simg.c
@@ -22,6 +22,7 @@
 #include <sys/types.h>
 #include <sys/mman.h>
 #include <fcntl.h>
+#include <inttypes.h>
 #include <libgen.h>
 #include <unistd.h>
 
@@ -86,13 +87,13 @@ static int read_ext(int fd)
 
 	if (verbose) {
 		printf("Found filesystem with parameters:\n");
-		printf("    Size: %llu\n", info.len);
+		printf("    Size: %"PRIu64"\n", info.len);
 		printf("    Block size: %d\n", info.block_size);
 		printf("    Blocks per group: %d\n", info.blocks_per_group);
 		printf("    Inodes per group: %d\n", info.inodes_per_group);
 		printf("    Inode size: %d\n", info.inode_size);
 		printf("    Label: %s\n", info.label);
-		printf("    Blocks: %llu\n", aux_info.len_blocks);
+		printf("    Blocks: %"PRIu64"\n", aux_info.len_blocks);
 		printf("    Block groups: %d\n", aux_info.groups);
 		printf("    Reserved block group size: %d\n", info.bg_desc_reserve_blocks);
 		printf("    Used %d/%d inodes and %d/%d blocks\n",

ext4_utils/ext4fixup.c
@@ -25,6 +25,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <inttypes.h>
 #include <unistd.h>
 
 #ifndef USE_MINGW
@@ -235,13 +236,13 @@ static int read_ext(int fd)
 
     if (verbose) {
         printf("Found filesystem with parameters:\n");
-        printf("    Size: %llu\n", info.len);
+        printf("    Size: %"PRIu64"\n", info.len);
         printf("    Block size: %d\n", info.block_size);
         printf("    Blocks per group: %d\n", info.blocks_per_group);
         printf("    Inodes per group: %d\n", info.inodes_per_group);
         printf("    Inode size: %d\n", info.inode_size);
         printf("    Label: %s\n", info.label);
-        printf("    Blocks: %llu\n", aux_info.len_blocks);
+        printf("    Blocks: %"PRIu64"\n", aux_info.len_blocks);
         printf("    Block groups: %d\n", aux_info.groups);
         printf("    Reserved block group size: %d\n", info.bg_desc_reserve_blocks);
         printf("    Used %d/%d inodes and %d/%d blocks\n",

ext4_utils/make_ext4fs.c
@@ -26,6 +26,7 @@
 #include <assert.h>
 #include <dirent.h>
 #include <fcntl.h>
+#include <inttypes.h>
 #include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -531,7 +532,7 @@ int make_ext4fs_internal(int fd, const char *_directory,
 	info.bg_desc_reserve_blocks = compute_bg_desc_reserve_blocks();
 
 	printf("Creating filesystem with parameters:\n");
-	printf("    Size: %llu\n", info.len);
+	printf("    Size: %"PRIu64"\n", info.len);
 	printf("    Block size: %d\n", info.block_size);
 	printf("    Blocks per group: %d\n", info.blocks_per_group);
 	printf("    Inodes per group: %d\n", info.inodes_per_group);
@@ -541,7 +542,7 @@ int make_ext4fs_internal(int fd, const char *_directory,
 
 	ext4_create_fs_aux_info();
 
-	printf("    Blocks: %llu\n", aux_info.len_blocks);
+	printf("    Blocks: %"PRIu64"\n", aux_info.len_blocks);
 	printf("    Block groups: %d\n", aux_info.groups);
 	printf("    Reserved block group size: %d\n", info.bg_desc_reserve_blocks);
 