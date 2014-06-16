136c149632302337df2d225bb85ee3db3d7e1971
JP Abgrall <jpa@google.com>
2014-06-16 11:08:45 -0700

    f2fs utils: Add mkf2fsuserimg.sh and make_f2fs
    This is used to generate empty userdata.img files.
    Normally, "fastboot format userdata" should be used.
    
    Caveats:
     - It only builds an empty sparse image.
     - windows/mac unsupported
    
    Change-Id: I5fbd9724fdd3f4513e7c91f123fc5fffcb475012
    Signed-off-by: JP Abgrall <jpa@google.com>

--- f2fs_utils/Android.mk
+++ f2fs_utils/Android.mk
@@ -34,12 +34,22 @@ LOCAL_STATIC_LIBRARIES := \
 LOCAL_C_INCLUDES := external/f2fs-tools/include external/f2fs-tools/mkfs
 include $(BUILD_HOST_STATIC_LIBRARY)
 
-
 #
 # -- All host/targets excluding windows
 #
 
 ifneq ($(HOST_OS),windows)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := make_f2fs_main.c
+LOCAL_MODULE := make_f2fs
+LOCAL_STATIC_LIBRARIES += \
+    libf2fs_utils_host \
+    libf2fs_dlutils_host
+# libf2fs_dlutils_host will dlopen("libf2fs_fmt_host_dyn")
+LOCAL_LDLIBS := -ldl
+LOCAL_SHARED_LIBRARIES := libf2fs_fmt_host_dyn
+include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libf2fs_dlutils
@@ -62,6 +72,16 @@ LOCAL_C_INCLUDES := external/f2fs-tools/include external/f2fs-tools/mkfs
 LOCAL_STATIC_LIBRARIES := \
     libsparse_static
 include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mkf2fsuserimg.sh
+LOCAL_SRC_FILES := mkf2fsuserimg.sh
+LOCAL_MODULE_CLASS := EXECUTABLES
+# We don't need any additional suffix.
+LOCAL_MODULE_SUFFIX :=
+LOCAL_BUILT_MODULE_STEM := $(notdir $(LOCAL_SRC_FILES))
+LOCAL_IS_HOST_MODULE := true
+include $(BUILD_PREBUILT)
 
 endif
 
