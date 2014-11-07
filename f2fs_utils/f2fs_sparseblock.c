9c84e7243e25059bc40f4cc28694f692502de952
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-07 01:21:24 +0000

    f2fs_utils: Add a static libf2fs_sparseblock for minvold

--- f2fs_utils/f2fs_sparseblock.c
+++ f2fs_utils/f2fs_sparseblock.c
@@ -577,6 +577,7 @@ int copy_used(u64 pos, void *data)
     return 0;
 }
 
+#ifndef STATIC_LIB
 int main(int argc, char **argv)
 {
     if (argc != 3)
@@ -620,3 +621,4 @@ int main(int argc, char **argv)
     close(outfd);
     return 0;
 }
+#endif
