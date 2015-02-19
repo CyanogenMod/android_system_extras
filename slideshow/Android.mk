a0f13e0a10ecf306fa88b5fbe68dddf81bf8cd8b
Sami Tolvanen <samitolvanen@google.com>
2015-02-19 10:44:12 +0000

    Add a program to display a slideshow at early boot
    Add a small program to display a slideshow of images (specified
    on the command line) on the framebuffer device. Images are switched
    after a timeout, or user input. The timeout can be optionally set
    using the -t command line parameter and defaults to 5s.
    
    If user input occurred, the program will wait for the power button
    to be pressed after the last image before exiting. To avoid draining
    the battery after an accidental reboot, the program exists after 30s
    even if the power button is not pressed.
    
    Change-Id: I58c79a7f3ac747eec0d73a10f018d3d8ade9df7d
    (Moved from I743c68b5f38038abcaaf5ae08233fa72fd823c8e)

---
+++ slideshow/Android.mk
@@ -0,0 +1,16 @@
+# Copyright 2015 The Android Open Source Project
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := slideshow.cpp
+LOCAL_MODULE := slideshow
+LOCAL_MODULE_TAGS := optional
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT_SBIN)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
+
+LOCAL_CFLAGS := -D__STDC_LIMIT_MACROS -Werror
+LOCAL_C_INCLUDES := bootable/recovery
+LOCAL_STATIC_LIBRARIES := libminui libpng libz libutils libstdc++ libcutils liblog libm libc
+include $(BUILD_EXECUTABLE)
