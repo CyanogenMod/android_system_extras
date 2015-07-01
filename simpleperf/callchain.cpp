jbeich/platform_system_extras

Simpleperf: support callgraph option in report command.
Bug: 19483574
Change-Id: I7c5558a71ea15650c1d3e7afa268b3fbc11543d7

simpleperf/callchain.cpp
@@ -1,0 +1,125 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "callchain.h"
+
+#include <queue>
+#include <base/logging.h>
+#include "sample_tree.h"
+
+static bool MatchSample(const SampleEntry* sample1, const SampleEntry* sample2) {
+  return (sample1->symbol->name == sample2->symbol->name);
+}
+
+static size_t MatchSamples(const std::vector<SampleEntry*>& samples1,
+                           const std::vector<SampleEntry*>& samples2, size_t samples2_start) {
+  size_t i, j;
+  for (i = 0, j = samples2_start; i < samples1.size() && j < samples2.size(); ++i, ++j) {
+    if (!MatchSample(samples1[i], samples2[j])) {
+      break;
+    }
+  }
+  return i;
+}
+
+static CallChainNode* SelectMatchingNode(const std::vector<std::unique_ptr<CallChainNode>>& nodes,
+                                         const SampleEntry* sample) {
+  for (auto& node : nodes) {
+    if (MatchSample(node->chain.front(), sample)) {
+      return node.get();
+    }
+  }
+  return nullptr;
+}
+
+static std::unique_ptr<CallChainNode> AllocateNode(const std::vector<SampleEntry*>& chain,
+                                                   size_t chain_start, uint64_t period,
+                                                   uint64_t children_period) {
+  std::unique_ptr<CallChainNode> node(new CallChainNode);
+  for (size_t i = chain_start; i < chain.size(); ++i) {
+    node->chain.push_back(chain[i]);
+  }
+  node->period = period;
+  node->children_period = children_period;
+  return node;
+}
+
+static void SplitNode(CallChainNode* parent, size_t parent_length) {
+  std::unique_ptr<CallChainNode> child =
+      AllocateNode(parent->chain, parent_length, parent->period, parent->children_period);
+  child->children = std::move(parent->children);
+  parent->period = 0;
+  parent->children_period = child->period + child->children_period;
+  parent->chain.resize(parent_length);
+  parent->children.clear();
+  parent->children.push_back(std::move(child));
+}
+
+void CallChainRoot::AddCallChain(const std::vector<SampleEntry*>& callchain, uint64_t period) {
+  children_period += period;
+  CallChainNode* p = SelectMatchingNode(children, callchain[0]);
+  if (p == nullptr) {
+    std::unique_ptr<CallChainNode> new_node = AllocateNode(callchain, 0, period, 0);
+    children.push_back(std::move(new_node));
+    return;
+  }
+  size_t callchain_pos = 0;
+  while (true) {
+    size_t match_count = MatchSamples(p->chain, callchain, callchain_pos);
+    CHECK_GT(match_count, 0u);
+    callchain_pos += match_count;
+    bool find_child = true;
+    if (match_count < p->chain.size()) {
+      SplitNode(p, match_count);
+      find_child = false;  // No need to find matching node in p->children.
+    }
+    if (callchain_pos == callchain.size()) {
+      p->period += period;
+      return;
+    }
+    p->children_period += period;
+    if (find_child) {
+      CallChainNode* np = SelectMatchingNode(p->children, callchain[callchain_pos]);
+      if (np != nullptr) {
+        p = np;
+        continue;
+      }
+    }
+    std::unique_ptr<CallChainNode> new_node = AllocateNode(callchain, callchain_pos, period, 0);
+    p->children.push_back(std::move(new_node));
+    break;
+  }
+}
+
+static bool CompareNodeByPeriod(const std::unique_ptr<CallChainNode>& n1,
+                                const std::unique_ptr<CallChainNode>& n2) {
+  uint64_t period1 = n1->period + n1->children_period;
+  uint64_t period2 = n2->period + n2->children_period;
+  return period1 > period2;
+}
+
+void CallChainRoot::SortByPeriod() {
+  std::queue<std::vector<std::unique_ptr<CallChainNode>>*> queue;
+  queue.push(&children);
+  while (!queue.empty()) {
+    std::vector<std::unique_ptr<CallChainNode>>* v = queue.front();
+    queue.pop();
+    std::sort(v->begin(), v->end(), CompareNodeByPeriod);
+    for (auto& node : *v) {
+      queue.push(&node->children);
+    }
+  }
+}

simpleperf/cmd_report.cpp
@@ -238,6 +238,7 @@ class ReportCommand : public Command {
             "                  the instruction addresses. Only valid for perf.data recorded with\n"
             "                  -b/-j option.\n"
             "    --children    Print the overhead accumulated by appearing in the callchain.\n"
+            "    -g            Print call graph.\n"
             "    -i <file>     Specify path of record file, default is perf.data.\n"
             "    -n            Print the sample count for each item.\n"
             "    --no-demangle        Don't demangle symbol names.\n"
@@ -249,7 +250,8 @@ class ReportCommand : public Command {
             "    --symfs <dir>  Look for files with symbols relative to this directory.\n"),
         record_filename_("perf.data"),
         use_branch_address_(false),
-        accumulate_callchain_(false) {
+        accumulate_callchain_(false),
+        print_callgraph_(false) {
     compare_sample_func_t compare_sample_callback = std::bind(
         &ReportCommand::CompareSampleEntry, this, std::placeholders::_1, std::placeholders::_2);
     sample_tree_ = std::unique_ptr<SampleTree>(new SampleTree(compare_sample_callback));
@@ -270,6 +272,7 @@ class ReportCommand : public Command {
   void CollectReportEntryWidth(const SampleEntry& sample);
   void PrintReportHeader();
   void PrintReportEntry(const SampleEntry& sample);
+  void PrintCallGraph(const SampleEntry& sample);
 
   std::string record_filename_;
   std::unique_ptr<RecordFileReader> record_file_reader_;
@@ -280,6 +283,7 @@ class ReportCommand : public Command {
   bool use_branch_address_;
   std::string record_cmdline_;
   bool accumulate_callchain_;
+  bool print_callgraph_;
 };
 
 bool ReportCommand::Run(const std::vector<std::string>& args) {
@@ -312,6 +316,9 @@ bool ReportCommand::ParseOptions(const std::vector<std::string>& args) {
     if (args[i] == "-b") {
       use_branch_address_ = true;
     } else if (args[i] == "--children") {
+      accumulate_callchain_ = true;
+    } else if (args[i] == "-g") {
+      print_callgraph_ = true;
       accumulate_callchain_ = true;
     } else if (args[i] == "-i") {
       if (!NextArgumentOrError(args, &i)) {
@@ -471,6 +478,7 @@ void ReportCommand::ProcessSampleRecord(const SampleRecord& r) {
       std::vector<SampleEntry*> callchain;
       callchain.push_back(sample);
       const std::vector<uint64_t>& ips = r.callchain_data.ips;
+      bool first_ip = true;
       for (auto& ip : ips) {
         if (ip >= PERF_CONTEXT_MAX) {
           switch (ip) {
@@ -484,10 +492,30 @@ void ReportCommand::ProcessSampleRecord(const SampleRecord& r) {
               LOG(ERROR) << "Unexpected perf_context in callchain: " << ip;
           }
         } else {
-          sample =
+          if (first_ip) {
+            // Remove duplication with sampled ip.
+            if (ip == r.ip_data.ip) {
+              continue;
+            }
+            first_ip = false;
+          }
+          SampleEntry* sample =
               sample_tree_->AddCallChainSample(r.tid_data.pid, r.tid_data.tid, ip, r.time_data.time,
                                                r.period_data.period, in_kernel, callchain);
           callchain.push_back(sample);
+        }
+      }
+      if (print_callgraph_) {
+        std::set<SampleEntry*> added_set;
+        while (callchain.size() >= 2) {
+          SampleEntry* sample = callchain[0];
+          callchain.erase(callchain.begin());
+          // Add only once for recursive calls on callchain.
+          if (added_set.find(sample) != added_set.end()) {
+            continue;
+          }
+          added_set.insert(sample);
+          sample_tree_->InsertCallChainForSample(sample, callchain, r.period_data.period);
         }
       }
     }
@@ -567,6 +595,49 @@ void ReportCommand::PrintReportEntry(const SampleEntry& sample) {
     } else {
       printf("%s\n", item->Show(sample).c_str());
     }
+  }
+  if (print_callgraph_) {
+    PrintCallGraph(sample);
+  }
+}
+
+static void PrintCallGraphEntry(size_t depth, std::string prefix,
+                                const std::unique_ptr<CallChainNode>& node, uint64_t parent_period,
+                                bool last) {
+  if (depth > 20) {
+    LOG(WARNING) << "stop callgraph at depth " << depth;
+    return;
+  }
+  prefix += "|";
+  printf("%s\n", prefix.c_str());
+  if (last) {
+    prefix.back() = ' ';
+  }
+  std::string percentage_s = "-- ";
+  if (node->period + node->children_period != parent_period) {
+    double percentage = 100.0 * (node->period + node->children_period) / parent_period;
+    percentage_s = android::base::StringPrintf("--%.2lf%%-- ", percentage);
+  }
+  printf("%s%s%s\n", prefix.c_str(), percentage_s.c_str(), node->chain[0]->symbol->name.c_str());
+  prefix.append(percentage_s.size(), ' ');
+  for (size_t i = 1; i < node->chain.size(); ++i) {
+    printf("%s%s\n", prefix.c_str(), node->chain[i]->symbol->name.c_str());
+  }
+
+  for (size_t i = 0; i < node->children.size(); ++i) {
+    PrintCallGraphEntry(depth + 1, prefix, node->children[i], node->children_period,
+                        (i + 1 == node->children.size()));
+  }
+}
+
+void ReportCommand::PrintCallGraph(const SampleEntry& sample) {
+  std::string prefix = "       ";
+  printf("%s|\n", prefix.c_str());
+  printf("%s-- %s\n", prefix.c_str(), sample.symbol->name.c_str());
+  prefix.append(3, ' ');
+  for (size_t i = 0; i < sample.callchain.children.size(); ++i) {
+    PrintCallGraphEntry(1, prefix, sample.callchain.children[i], sample.callchain.children_period,
+                        (i + 1 == sample.callchain.children.size()));
   }
 }
 

CyanogenMod/android_system_extras

simpleperf/callchain.cpp
@@ -1,0 +1,125 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "callchain.h"
+
+#include <queue>
+#include <base/logging.h>
+#include "sample_tree.h"
+
+static bool MatchSampleByName(const SampleEntry* sample1, const SampleEntry* sample2) {
+  return (sample1->symbol->name == sample2->symbol->name);
+}
+
+static size_t GetMatchingLengthInNode(const CallChainNode* node,
+                                      const std::vector<SampleEntry*>& chain, size_t chain_start) {
+  size_t i, j;
+  for (i = 0, j = chain_start; i < node->chain.size() && j < chain.size(); ++i, ++j) {
+    if (!MatchSampleByName(node->chain[i], chain[j])) {
+      break;
+    }
+  }
+  return i;
+}
+
+static CallChainNode* FindMatchingNode(const std::vector<std::unique_ptr<CallChainNode>>& nodes,
+                                       const SampleEntry* sample) {
+  for (auto& node : nodes) {
+    if (MatchSampleByName(node->chain.front(), sample)) {
+      return node.get();
+    }
+  }
+  return nullptr;
+}
+
+static std::unique_ptr<CallChainNode> AllocateNode(const std::vector<SampleEntry*>& chain,
+                                                   size_t chain_start, uint64_t period,
+                                                   uint64_t children_period) {
+  std::unique_ptr<CallChainNode> node(new CallChainNode);
+  for (size_t i = chain_start; i < chain.size(); ++i) {
+    node->chain.push_back(chain[i]);
+  }
+  node->period = period;
+  node->children_period = children_period;
+  return node;
+}
+
+static void SplitNode(CallChainNode* parent, size_t parent_length) {
+  std::unique_ptr<CallChainNode> child =
+      AllocateNode(parent->chain, parent_length, parent->period, parent->children_period);
+  child->children = std::move(parent->children);
+  parent->period = 0;
+  parent->children_period = child->period + child->children_period;
+  parent->chain.resize(parent_length);
+  parent->children.clear();
+  parent->children.push_back(std::move(child));
+}
+
+void CallChainRoot::AddCallChain(const std::vector<SampleEntry*>& callchain, uint64_t period) {
+  children_period += period;
+  CallChainNode* p = FindMatchingNode(children, callchain[0]);
+  if (p == nullptr) {
+    std::unique_ptr<CallChainNode> new_node = AllocateNode(callchain, 0, period, 0);
+    children.push_back(std::move(new_node));
+    return;
+  }
+  size_t callchain_pos = 0;
+  while (true) {
+    size_t match_length = GetMatchingLengthInNode(p, callchain, callchain_pos);
+    CHECK_GT(match_length, 0u);
+    callchain_pos += match_length;
+    bool find_child = true;
+    if (match_length < p->chain.size()) {
+      SplitNode(p, match_length);
+      find_child = false;  // No need to find matching node in p->children.
+    }
+    if (callchain_pos == callchain.size()) {
+      p->period += period;
+      return;
+    }
+    p->children_period += period;
+    if (find_child) {
+      CallChainNode* np = FindMatchingNode(p->children, callchain[callchain_pos]);
+      if (np != nullptr) {
+        p = np;
+        continue;
+      }
+    }
+    std::unique_ptr<CallChainNode> new_node = AllocateNode(callchain, callchain_pos, period, 0);
+    p->children.push_back(std::move(new_node));
+    break;
+  }
+}
+
+static bool CompareNodeByPeriod(const std::unique_ptr<CallChainNode>& n1,
+                                const std::unique_ptr<CallChainNode>& n2) {
+  uint64_t period1 = n1->period + n1->children_period;
+  uint64_t period2 = n2->period + n2->children_period;
+  return period1 > period2;
+}
+
+void CallChainRoot::SortByPeriod() {
+  std::queue<std::vector<std::unique_ptr<CallChainNode>>*> queue;
+  queue.push(&children);
+  while (!queue.empty()) {
+    std::vector<std::unique_ptr<CallChainNode>>* v = queue.front();
+    queue.pop();
+    std::sort(v->begin(), v->end(), CompareNodeByPeriod);
+    for (auto& node : *v) {
+      queue.push(&node->children);
+    }
+  }
+}

simpleperf/cmd_report.cpp
@@ -238,6 +238,7 @@ class ReportCommand : public Command {
             "                  the instruction addresses. Only valid for perf.data recorded with\n"
             "                  -b/-j option.\n"
             "    --children    Print the overhead accumulated by appearing in the callchain.\n"
+            "    -g            Print call graph.\n"
             "    -i <file>     Specify path of record file, default is perf.data.\n"
             "    -n            Print the sample count for each item.\n"
             "    --no-demangle        Don't demangle symbol names.\n"
@@ -249,7 +250,8 @@ class ReportCommand : public Command {
             "    --symfs <dir>  Look for files with symbols relative to this directory.\n"),
         record_filename_("perf.data"),
         use_branch_address_(false),
-        accumulate_callchain_(false) {
+        accumulate_callchain_(false),
+        print_callgraph_(false) {
     compare_sample_func_t compare_sample_callback = std::bind(
         &ReportCommand::CompareSampleEntry, this, std::placeholders::_1, std::placeholders::_2);
     sample_tree_ = std::unique_ptr<SampleTree>(new SampleTree(compare_sample_callback));
@@ -270,6 +272,7 @@ class ReportCommand : public Command {
   void CollectReportEntryWidth(const SampleEntry& sample);
   void PrintReportHeader();
   void PrintReportEntry(const SampleEntry& sample);
+  void PrintCallGraph(const SampleEntry& sample);
 
   std::string record_filename_;
   std::unique_ptr<RecordFileReader> record_file_reader_;
@@ -280,6 +283,7 @@ class ReportCommand : public Command {
   bool use_branch_address_;
   std::string record_cmdline_;
   bool accumulate_callchain_;
+  bool print_callgraph_;
 };
 
 bool ReportCommand::Run(const std::vector<std::string>& args) {
@@ -312,6 +316,9 @@ bool ReportCommand::ParseOptions(const std::vector<std::string>& args) {
     if (args[i] == "-b") {
       use_branch_address_ = true;
     } else if (args[i] == "--children") {
+      accumulate_callchain_ = true;
+    } else if (args[i] == "-g") {
+      print_callgraph_ = true;
       accumulate_callchain_ = true;
     } else if (args[i] == "-i") {
       if (!NextArgumentOrError(args, &i)) {
@@ -471,6 +478,7 @@ void ReportCommand::ProcessSampleRecord(const SampleRecord& r) {
       std::vector<SampleEntry*> callchain;
       callchain.push_back(sample);
       const std::vector<uint64_t>& ips = r.callchain_data.ips;
+      bool first_ip = true;
       for (auto& ip : ips) {
         if (ip >= PERF_CONTEXT_MAX) {
           switch (ip) {
@@ -484,10 +492,30 @@ void ReportCommand::ProcessSampleRecord(const SampleRecord& r) {
               LOG(ERROR) << "Unexpected perf_context in callchain: " << ip;
           }
         } else {
-          sample =
+          if (first_ip) {
+            // Remove duplication with sampled ip.
+            if (ip == r.ip_data.ip) {
+              continue;
+            }
+            first_ip = false;
+          }
+          SampleEntry* sample =
               sample_tree_->AddCallChainSample(r.tid_data.pid, r.tid_data.tid, ip, r.time_data.time,
                                                r.period_data.period, in_kernel, callchain);
           callchain.push_back(sample);
+        }
+      }
+      if (print_callgraph_) {
+        std::set<SampleEntry*> added_set;
+        while (callchain.size() >= 2) {
+          SampleEntry* sample = callchain[0];
+          callchain.erase(callchain.begin());
+          // Add only once for recursive calls on callchain.
+          if (added_set.find(sample) != added_set.end()) {
+            continue;
+          }
+          added_set.insert(sample);
+          sample_tree_->InsertCallChainForSample(sample, callchain, r.period_data.period);
         }
       }
     }
@@ -567,6 +595,49 @@ void ReportCommand::PrintReportEntry(const SampleEntry& sample) {
     } else {
       printf("%s\n", item->Show(sample).c_str());
     }
+  }
+  if (print_callgraph_) {
+    PrintCallGraph(sample);
+  }
+}
+
+static void PrintCallGraphEntry(size_t depth, std::string prefix,
+                                const std::unique_ptr<CallChainNode>& node, uint64_t parent_period,
+                                bool last) {
+  if (depth > 20) {
+    LOG(WARNING) << "truncated callgraph at depth " << depth;
+    return;
+  }
+  prefix += "|";
+  printf("%s\n", prefix.c_str());
+  if (last) {
+    prefix.back() = ' ';
+  }
+  std::string percentage_s = "-- ";
+  if (node->period + node->children_period != parent_period) {
+    double percentage = 100.0 * (node->period + node->children_period) / parent_period;
+    percentage_s = android::base::StringPrintf("--%.2lf%%-- ", percentage);
+  }
+  printf("%s%s%s\n", prefix.c_str(), percentage_s.c_str(), node->chain[0]->symbol->name.c_str());
+  prefix.append(percentage_s.size(), ' ');
+  for (size_t i = 1; i < node->chain.size(); ++i) {
+    printf("%s%s\n", prefix.c_str(), node->chain[i]->symbol->name.c_str());
+  }
+
+  for (size_t i = 0; i < node->children.size(); ++i) {
+    PrintCallGraphEntry(depth + 1, prefix, node->children[i], node->children_period,
+                        (i + 1 == node->children.size()));
+  }
+}
+
+void ReportCommand::PrintCallGraph(const SampleEntry& sample) {
+  std::string prefix = "       ";
+  printf("%s|\n", prefix.c_str());
+  printf("%s-- %s\n", prefix.c_str(), sample.symbol->name.c_str());
+  prefix.append(3, ' ');
+  for (size_t i = 0; i < sample.callchain.children.size(); ++i) {
+    PrintCallGraphEntry(1, prefix, sample.callchain.children[i], sample.callchain.children_period,
+                        (i + 1 == sample.callchain.children.size()));
   }
 }
 