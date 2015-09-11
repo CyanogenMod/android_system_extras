33c03d38298ff359d8829aea91bb02b26caab245
Christopher Ferris <cferris@google.com>
2015-09-11 15:34:23 -0700

    Tool to replay memory allocation recordings.
    Included with the tool are a set of app memory recordings that
    can be used to judge the abilities of the native memory
    allocator.
    
    Verified the full flow by running valgrind on the host version on all
    the current dumps and verifying that there are no leak, and no errors.
    
    Change-Id: I756224991f04b61ba26c692a30a37aad0c9fb163

---
+++ memory_replay/Android.mk
@@ -0,0 +1,72 @@
+LOCAL_PATH := $(call my-dir)
+
+memory_replay_src_files := \
+	Action.cpp \
+	LineBuffer.cpp \
+	NativeInfo.cpp \
+	Pointers.cpp \
+	Thread.cpp \
+	Threads.cpp \
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(memory_replay_src_files) main.cpp
+LOCAL_CFLAGS := -Wall -Wextra -Werror
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE := memory_replay
+LOCAL_MULTILIB := both
+LOCAL_MODULE_STEM_32 := $(LOCAL_MODULE)32
+LOCAL_MODULE_STEM_64 := $(LOCAL_MODULE)64
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(memory_replay_src_files) main.cpp
+LOCAL_CFLAGS := -Wall -Wextra -Werror
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE := memory_replay
+include $(BUILD_HOST_EXECUTABLE)
+
+memory_replay_test_src_files := \
+	tests/ActionTest.cpp \
+	tests/LineBufferTest.cpp \
+	tests/NativeInfoTest.cpp \
+	tests/PointersTest.cpp \
+	tests/ThreadTest.cpp \
+	tests/ThreadsTest.cpp \
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	$(memory_replay_src_files) \
+	$(memory_replay_test_src_files) \
+
+LOCAL_CFLAGS := -Wall -Wextra -Werror
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/tests
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE := memory_replay_tests
+
+LOCAL_SHARED_LIBRARIES := libbase
+
+LOCAL_MULTILIB := both
+LOCAL_MODULE_STEM_32 := $(LOCAL_MODULE)32
+LOCAL_MODULE_STEM_64 := $(LOCAL_MODULE)64
+include $(BUILD_NATIVE_TEST)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	$(memory_replay_src_files) \
+	$(memory_replay_test_src_files) \
+
+LOCAL_CFLAGS := -Wall -Wextra -Werror
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/tests
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE := memory_replay_tests
+
+LOCAL_SHARED_LIBRARIES := libbase
+
+include $(BUILD_HOST_NATIVE_TEST)
+
+memory_replay_src_files :=
+memory_replay_test_src_files :=
