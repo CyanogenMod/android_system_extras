f5dbe9fc1c4466a539b4d6431de56fb4a2ef95e3
Gianmarco Reverberi <gianmarco.reverberi@gmail.com>
2015-10-13 23:14:23 +0200

    ext4_utils: use libcutils
    Change-Id: I390c21749f8ed602d21ada8f8bdef00a63509277

--- ext4_utils/Android.mk
+++ ext4_utils/Android.mk
@@ -77,8 +77,10 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(libext4_utils_src_files) \
     ext4_crypt_init_extensions.cpp
 LOCAL_MODULE := libext4_utils_static
+LOCAL_SHARED_LIBRARIES := \
+    libcutils
 LOCAL_STATIC_LIBRARIES := \
-    libsparse_static
+    libsparse_static \
 include $(BUILD_STATIC_LIBRARY)
 
 
