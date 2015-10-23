jbeich/platform_system_extras

Simpleperf: fix some unknown symbols for report result.
Using debug shared libraries in /usr/lib/debug on linux host.
Match ip addresses with symbols by symbols' virtual addresses instead of
file offsets in elf file. Because symbols' file offsets in debug shared
libraries are different from those in original shared libraries.

Fix overlapped maps.

Bug: 24716851
Change-Id: I9cb64958c4de5c7a6c77c3febc5f689cf2df650f

simpleperf/dso.cpp
@@ -134,7 +134,8 @@ std::unique_ptr<Dso> Dso::CreateDso(DsoType dso_type, const std::string& dso_pat
   return std::unique_ptr<Dso>(new Dso(dso_type, path));
 }
 
-Dso::Dso(DsoType type, const std::string& path) : type_(type), path_(path), is_loaded_(false) {
+Dso::Dso(DsoType type, const std::string& path)
+    : type_(type), path_(path), min_vaddr_(std::numeric_limits<uint64_t>::max()), is_loaded_(false) {
   dso_count_++;
 }
 
@@ -154,7 +155,7 @@ std::string Dso::GetAccessiblePath() const {
   return symfs_dir_ + path_;
 }
 
-const Symbol* Dso::FindSymbol(uint64_t offset_in_dso) {
+const Symbol* Dso::FindSymbol(uint64_t vaddr_in_dso) {
   if (!is_loaded_) {
     is_loaded_ = true;
     if (!Load()) {
@@ -163,15 +164,30 @@ const Symbol* Dso::FindSymbol(uint64_t offset_in_dso) {
     }
   }
 
-  auto it = std::upper_bound(symbols_.begin(), symbols_.end(), Symbol("", offset_in_dso, 0),
+  auto it = std::upper_bound(symbols_.begin(), symbols_.end(), Symbol("", vaddr_in_dso, 0),
                              SymbolComparator());
   if (it != symbols_.begin()) {
     --it;
-    if (it->addr <= offset_in_dso && it->addr + it->len > offset_in_dso) {
+    if (it->addr <= vaddr_in_dso && it->addr + it->len > vaddr_in_dso) {
       return &*it;
     }
   }
   return nullptr;
+}
+
+uint64_t Dso::MinVirtualAddress() {
+  if (min_vaddr_ == std::numeric_limits<uint64_t>::max()) {
+    min_vaddr_ = 0;
+    if (type_ == DSO_ELF_FILE) {
+      BuildId build_id = GetExpectedBuildId(GetAccessiblePath());
+
+      uint64_t addr;
+      if (ReadMinVirtualAddressFromElfFile(GetAccessiblePath(), build_id, &addr)) {
+        min_vaddr_ = addr;
+      }
+    }
+  }
+  return min_vaddr_;
 }
 
 bool Dso::Load() {
@@ -250,7 +266,7 @@ bool Dso::LoadKernel() {
 void Dso::ElfFileSymbolCallback(const ElfFileSymbol& elf_symbol, Dso* dso,
                                 bool (*filter)(const ElfFileSymbol&)) {
   if (filter(elf_symbol)) {
-    dso->InsertSymbol(Symbol(elf_symbol.name, elf_symbol.start_in_file, elf_symbol.len));
+    dso->InsertSymbol(Symbol(elf_symbol.name, elf_symbol.vaddr, elf_symbol.len));
   }
 }
 
@@ -272,11 +288,21 @@ static bool SymbolFilterForDso(const ElfFileSymbol& elf_symbol) {
 }
 
 bool Dso::LoadElfFile() {
-  BuildId build_id = GetExpectedBuildId(path_);
-  ParseSymbolsFromElfFile(
-      symfs_dir_ + path_, build_id,
-      std::bind(ElfFileSymbolCallback, std::placeholders::_1, this, SymbolFilterForDso));
-  return true;
+  bool loaded = false;
+  BuildId build_id = GetExpectedBuildId(GetAccessiblePath());
+
+  if (symfs_dir_.empty()) {
+    // Linux host can store debug shared libraries in /usr/lib/debug.
+    loaded = ParseSymbolsFromElfFile(
+        "/usr/lib/debug" + path_, build_id,
+        std::bind(ElfFileSymbolCallback, std::placeholders::_1, this, SymbolFilterForDso));
+  }
+  if (!loaded) {
+    loaded = ParseSymbolsFromElfFile(
+        GetAccessiblePath(), build_id,
+        std::bind(ElfFileSymbolCallback, std::placeholders::_1, this, SymbolFilterForDso));
+  }
+  return loaded;
 }
 
 void Dso::InsertSymbol(const Symbol& symbol) {

simpleperf/read_elf.cpp
@@ -18,7 +18,10 @@
 
 #include <stdio.h>
 #include <string.h>
+
 #include <algorithm>
+#include <limits>
+
 #include <base/file.h>
 #include <base/logging.h>
 
@@ -168,10 +171,9 @@ void ParseSymbolsFromELFFile(const llvm::object::ELFFile<ELFT>* elf,
       continue;
     }
     symbol.vaddr = elf_symbol.st_value;
-    symbol.start_in_file = elf_symbol.st_value - shdr->sh_addr + shdr->sh_offset;
-    if ((symbol.start_in_file & 1) != 0 && is_arm) {
+    if ((symbol.vaddr & 1) != 0 && is_arm) {
       // Arm sets bit 0 to mark it as thumb code, remove the flag.
-      symbol.start_in_file &= ~1;
+      symbol.vaddr &= ~1;
     }
     symbol.len = elf_symbol.st_size;
     int type = elf_symbol.getType();
@@ -196,18 +198,18 @@ void ParseSymbolsFromELFFile(const llvm::object::ELFFile<ELFT>* elf,
   }
 }
 
-bool ParseSymbolsFromElfFile(const std::string& filename, const BuildId& expected_build_id,
-                             std::function<void(const ElfFileSymbol&)> callback) {
-  auto owning_binary = llvm::object::createBinary(llvm::StringRef(filename));
+static llvm::object::ObjectFile* GetObjectFile(
+    llvm::ErrorOr<llvm::object::OwningBinary<llvm::object::Binary>>& owning_binary,
+    const std::string& filename, const BuildId& expected_build_id) {
   if (owning_binary.getError()) {
     PLOG(DEBUG) << "can't open file '" << filename << "'";
-    return false;
+    return nullptr;
   }
   llvm::object::Binary* binary = owning_binary.get().getBinary();
   auto obj = llvm::dyn_cast<llvm::object::ObjectFile>(binary);
   if (obj == nullptr) {
     LOG(DEBUG) << filename << " is not an object file";
-    return false;
+    return nullptr;
   }
   if (!expected_build_id.IsEmpty()) {
     BuildId real_build_id;
@@ -217,8 +219,18 @@ bool ParseSymbolsFromElfFile(const std::string& filename, const BuildId& expecte
                << "): expected " << expected_build_id.ToString() << ", real "
                << real_build_id.ToString();
     if (!result) {
-      return false;
+      return nullptr;
     }
+  }
+  return obj;
+}
+
+bool ParseSymbolsFromElfFile(const std::string& filename, const BuildId& expected_build_id,
+                             std::function<void(const ElfFileSymbol&)> callback) {
+  auto owning_binary = llvm::object::createBinary(llvm::StringRef(filename));
+  llvm::object::ObjectFile* obj = GetObjectFile(owning_binary, filename, expected_build_id);
+  if (obj == nullptr) {
+    return false;
   }
 
   if (auto elf = llvm::dyn_cast<llvm::object::ELF32LEObjectFile>(obj)) {
@@ -230,4 +242,44 @@ bool ParseSymbolsFromElfFile(const std::string& filename, const BuildId& expecte
     return false;
   }
   return true;
+}
+
+template <class ELFT>
+bool ReadMinVirtualAddress(const llvm::object::ELFFile<ELFT>* elf, uint64_t* p_vaddr) {
+  bool has_vaddr = false;
+  uint64_t min_addr = std::numeric_limits<uint64_t>::max();
+  for (auto it = elf->begin_program_headers(); it != elf->end_program_headers(); ++it) {
+    if (it->p_vaddr < min_addr) {
+      min_addr = it->p_vaddr;
+      has_vaddr = true;
+    }
+  }
+  if (has_vaddr) {
+    *p_vaddr = min_addr;
+  }
+  return has_vaddr;
+}
+
+bool ReadMinVirtualAddressFromElfFile(const std::string& filename, const BuildId& expected_build_id,
+                                      uint64_t* min_vaddr) {
+  auto owning_binary = llvm::object::createBinary(llvm::StringRef(filename));
+  llvm::object::ObjectFile* obj = GetObjectFile(owning_binary, filename, expected_build_id);
+  if (obj == nullptr) {
+    return false;
+  }
+
+  bool result = false;
+  if (auto elf = llvm::dyn_cast<llvm::object::ELF32LEObjectFile>(obj)) {
+    result = ReadMinVirtualAddress(elf->getELFFile(), min_vaddr);
+  } else if (auto elf = llvm::dyn_cast<llvm::object::ELF64LEObjectFile>(obj)) {
+    result = ReadMinVirtualAddress(elf->getELFFile(), min_vaddr);
+  } else {
+    LOG(ERROR) << "unknown elf format in file" << filename;
+    return false;
+  }
+
+  if (!result) {
+    LOG(ERROR) << "no program header in file " << filename;
+  }
+  return result;
 }

CyanogenMod/android_system_extras

simpleperf/dso.cpp
@@ -134,7 +134,8 @@ std::unique_ptr<Dso> Dso::CreateDso(DsoType dso_type, const std::string& dso_pat
   return std::unique_ptr<Dso>(new Dso(dso_type, path));
 }
 
-Dso::Dso(DsoType type, const std::string& path) : type_(type), path_(path), is_loaded_(false) {
+Dso::Dso(DsoType type, const std::string& path)
+    : type_(type), path_(path), min_vaddr_(std::numeric_limits<uint64_t>::max()), is_loaded_(false) {
   dso_count_++;
 }
 
@@ -154,7 +155,7 @@ std::string Dso::GetAccessiblePath() const {
   return symfs_dir_ + path_;
 }
 
-const Symbol* Dso::FindSymbol(uint64_t offset_in_dso) {
+const Symbol* Dso::FindSymbol(uint64_t vaddr_in_dso) {
   if (!is_loaded_) {
     is_loaded_ = true;
     if (!Load()) {
@@ -163,15 +164,30 @@ const Symbol* Dso::FindSymbol(uint64_t offset_in_dso) {
     }
   }
 
-  auto it = std::upper_bound(symbols_.begin(), symbols_.end(), Symbol("", offset_in_dso, 0),
+  auto it = std::upper_bound(symbols_.begin(), symbols_.end(), Symbol("", vaddr_in_dso, 0),
                              SymbolComparator());
   if (it != symbols_.begin()) {
     --it;
-    if (it->addr <= offset_in_dso && it->addr + it->len > offset_in_dso) {
+    if (it->addr <= vaddr_in_dso && it->addr + it->len > vaddr_in_dso) {
       return &*it;
     }
   }
   return nullptr;
+}
+
+uint64_t Dso::MinVirtualAddress() {
+  if (min_vaddr_ == std::numeric_limits<uint64_t>::max()) {
+    min_vaddr_ = 0;
+    if (type_ == DSO_ELF_FILE) {
+      BuildId build_id = GetExpectedBuildId(GetAccessiblePath());
+
+      uint64_t addr;
+      if (ReadMinExecutableVirtualAddressFromElfFile(GetAccessiblePath(), build_id, &addr)) {
+        min_vaddr_ = addr;
+      }
+    }
+  }
+  return min_vaddr_;
 }
 
 bool Dso::Load() {
@@ -250,7 +266,7 @@ bool Dso::LoadKernel() {
 void Dso::ElfFileSymbolCallback(const ElfFileSymbol& elf_symbol, Dso* dso,
                                 bool (*filter)(const ElfFileSymbol&)) {
   if (filter(elf_symbol)) {
-    dso->InsertSymbol(Symbol(elf_symbol.name, elf_symbol.start_in_file, elf_symbol.len));
+    dso->InsertSymbol(Symbol(elf_symbol.name, elf_symbol.vaddr, elf_symbol.len));
   }
 }
 
@@ -272,11 +288,21 @@ static bool SymbolFilterForDso(const ElfFileSymbol& elf_symbol) {
 }
 
 bool Dso::LoadElfFile() {
-  BuildId build_id = GetExpectedBuildId(path_);
-  ParseSymbolsFromElfFile(
-      symfs_dir_ + path_, build_id,
-      std::bind(ElfFileSymbolCallback, std::placeholders::_1, this, SymbolFilterForDso));
-  return true;
+  bool loaded = false;
+  BuildId build_id = GetExpectedBuildId(GetAccessiblePath());
+
+  if (symfs_dir_.empty()) {
+    // Linux host can store debug shared libraries in /usr/lib/debug.
+    loaded = ParseSymbolsFromElfFile(
+        "/usr/lib/debug" + path_, build_id,
+        std::bind(ElfFileSymbolCallback, std::placeholders::_1, this, SymbolFilterForDso));
+  }
+  if (!loaded) {
+    loaded = ParseSymbolsFromElfFile(
+        GetAccessiblePath(), build_id,
+        std::bind(ElfFileSymbolCallback, std::placeholders::_1, this, SymbolFilterForDso));
+  }
+  return loaded;
 }
 
 void Dso::InsertSymbol(const Symbol& symbol) {

simpleperf/read_elf.cpp
@@ -18,7 +18,10 @@
 
 #include <stdio.h>
 #include <string.h>
+
 #include <algorithm>
+#include <limits>
+
 #include <base/file.h>
 #include <base/logging.h>
 
@@ -168,10 +171,9 @@ void ParseSymbolsFromELFFile(const llvm::object::ELFFile<ELFT>* elf,
       continue;
     }
     symbol.vaddr = elf_symbol.st_value;
-    symbol.start_in_file = elf_symbol.st_value - shdr->sh_addr + shdr->sh_offset;
-    if ((symbol.start_in_file & 1) != 0 && is_arm) {
+    if ((symbol.vaddr & 1) != 0 && is_arm) {
       // Arm sets bit 0 to mark it as thumb code, remove the flag.
-      symbol.start_in_file &= ~1;
+      symbol.vaddr &= ~1;
     }
     symbol.len = elf_symbol.st_size;
     int type = elf_symbol.getType();
@@ -196,18 +198,18 @@ void ParseSymbolsFromELFFile(const llvm::object::ELFFile<ELFT>* elf,
   }
 }
 
-bool ParseSymbolsFromElfFile(const std::string& filename, const BuildId& expected_build_id,
-                             std::function<void(const ElfFileSymbol&)> callback) {
-  auto owning_binary = llvm::object::createBinary(llvm::StringRef(filename));
+static llvm::object::ObjectFile* GetObjectFile(
+    llvm::ErrorOr<llvm::object::OwningBinary<llvm::object::Binary>>& owning_binary,
+    const std::string& filename, const BuildId& expected_build_id) {
   if (owning_binary.getError()) {
     PLOG(DEBUG) << "can't open file '" << filename << "'";
-    return false;
+    return nullptr;
   }
   llvm::object::Binary* binary = owning_binary.get().getBinary();
   auto obj = llvm::dyn_cast<llvm::object::ObjectFile>(binary);
   if (obj == nullptr) {
     LOG(DEBUG) << filename << " is not an object file";
-    return false;
+    return nullptr;
   }
   if (!expected_build_id.IsEmpty()) {
     BuildId real_build_id;
@@ -217,8 +219,18 @@ bool ParseSymbolsFromElfFile(const std::string& filename, const BuildId& expecte
                << "): expected " << expected_build_id.ToString() << ", real "
                << real_build_id.ToString();
     if (!result) {
-      return false;
+      return nullptr;
     }
+  }
+  return obj;
+}
+
+bool ParseSymbolsFromElfFile(const std::string& filename, const BuildId& expected_build_id,
+                             std::function<void(const ElfFileSymbol&)> callback) {
+  auto owning_binary = llvm::object::createBinary(llvm::StringRef(filename));
+  llvm::object::ObjectFile* obj = GetObjectFile(owning_binary, filename, expected_build_id);
+  if (obj == nullptr) {
+    return false;
   }
 
   if (auto elf = llvm::dyn_cast<llvm::object::ELF32LEObjectFile>(obj)) {
@@ -230,4 +242,47 @@ bool ParseSymbolsFromElfFile(const std::string& filename, const BuildId& expecte
     return false;
   }
   return true;
+}
+
+template <class ELFT>
+bool ReadMinExecutableVirtualAddress(const llvm::object::ELFFile<ELFT>* elf, uint64_t* p_vaddr) {
+  bool has_vaddr = false;
+  uint64_t min_addr = std::numeric_limits<uint64_t>::max();
+  for (auto it = elf->begin_program_headers(); it != elf->end_program_headers(); ++it) {
+    if ((it->p_type == llvm::ELF::PT_LOAD) && (it->p_flags & llvm::ELF::PF_X)) {
+      if (it->p_vaddr < min_addr) {
+        min_addr = it->p_vaddr;
+        has_vaddr = true;
+      }
+    }
+  }
+  if (has_vaddr) {
+    *p_vaddr = min_addr;
+  }
+  return has_vaddr;
+}
+
+bool ReadMinExecutableVirtualAddressFromElfFile(const std::string& filename,
+                                                const BuildId& expected_build_id,
+                                                uint64_t* min_vaddr) {
+  auto owning_binary = llvm::object::createBinary(llvm::StringRef(filename));
+  llvm::object::ObjectFile* obj = GetObjectFile(owning_binary, filename, expected_build_id);
+  if (obj == nullptr) {
+    return false;
+  }
+
+  bool result = false;
+  if (auto elf = llvm::dyn_cast<llvm::object::ELF32LEObjectFile>(obj)) {
+    result = ReadMinExecutableVirtualAddress(elf->getELFFile(), min_vaddr);
+  } else if (auto elf = llvm::dyn_cast<llvm::object::ELF64LEObjectFile>(obj)) {
+    result = ReadMinExecutableVirtualAddress(elf->getELFFile(), min_vaddr);
+  } else {
+    LOG(ERROR) << "unknown elf format in file" << filename;
+    return false;
+  }
+
+  if (!result) {
+    LOG(ERROR) << "no program header in file " << filename;
+  }
+  return result;
 }