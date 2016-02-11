0a34d462046a77c7649a40226b06c2232a547635
Mark Salyzyn <salyzyn@google.com>
2016-02-11 14:20:24 -0800

    cts: android.kernel.config
    (cherry pick from commit 67522d05df61e7757e1ff08ba0b7ad0f19406ec0)
    
    - Added plumbing for CTS tests
    - Divide Required into CTS and gTest and
      Recommended into gTest
    - Added check of CONFIG_ANDROID_LOGGER
    - test typically completes after 3ms
    
    Bug: 19173869
    Change-Id: I13a92081bfa9b53abb69923b160b82e3be2142ab

--- tests/kernel.config/Android.mk
+++ tests/kernel.config/Android.mk
@@ -14,16 +14,57 @@ test_c_flags := \
     -fno-builtin \
     -std=gnu++11
 
-test_src_files := \
+# Required Tests
+cts_src_files := \
     multicast_test.cpp \
-    mmc_max_speed_test.cpp \
     pstore_test.cpp \
-    sysvipc_test.cpp
+    sysvipc_test.cpp \
+    logger_test.cpp
+
+# Required plus Recommended Tests
+test_src_files := \
+    $(cts_src_files) \
+    mmc_max_speed_test.cpp \
+
+cts_executable := CtsKernelConfigTestCases
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := kernel-config-unit-tests
 LOCAL_MODULE_TAGS := tests
-LOCAL_CFLAGS += $(test_c_flags)
+LOCAL_CFLAGS := $(test_c_flags)
+LOCAL_CFLAGS := -DHAS_KCMP
 LOCAL_SRC_FILES := $(test_src_files)
 include $(BUILD_NATIVE_TEST)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := $(cts_executable)
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS := $(test_c_flags)
+LOCAL_CFLAGS := -DHAS_KCMP
+LOCAL_SRC_FILES := $(cts_src_files)
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/nativetest
+LOCAL_MULTILIB := both
+LOCAL_MODULE_STEM_32 := $(LOCAL_MODULE)32
+LOCAL_MODULE_STEM_64 := $(LOCAL_MODULE)64
+LOCAL_STATIC_LIBRARIES := libgtest libgtest_main
+
+LOCAL_COMPATIBILITY_SUITE := cts_v2
+LOCAL_CTS_TEST_PACKAGE := android.kernel.config
+include $(BUILD_CTS_EXECUTABLE)
+
+ifeq ($(HOST_OS)-$(HOST_ARCH),$(filter $(HOST_OS)-$(HOST_ARCH),linux-x86 linux-x86_64))
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := $(cts_executable)_list
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS := $(test_c_flags)
+LOCAL_C_INCLUDES := external/gtest/include
+LOCAL_SRC_FILES := $(cts_src_files)
+LOCAL_MULTILIB := both
+LOCAL_MODULE_STEM_32 := $(LOCAL_MODULE)
+LOCAL_MODULE_STEM_64 := $(LOCAL_MODULE)64
+LOCAL_CXX_STL := libc++
+LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
+include $(BUILD_HOST_NATIVE_TEST)
+
+endif  # ifeq ($(HOST_OS)-$(HOST_ARCH),$(filter $(HOST_OS)-$(HOST_ARCH),linux-x86 linux-x86_64))
--- tests/kernel.config/sysvipc_test.cpp
+++ tests/kernel.config/sysvipc_test.cpp
@@ -14,22 +14,31 @@
  * limitations under the License.
  */
 #include <errno.h>
+#ifdef HAS_KCMP
 #include <linux/kcmp.h>
 #include <sys/syscall.h>
+#endif
 #include <unistd.h>
 
 #include <gtest/gtest.h>
 
+#ifdef HAS_KCMP
 int kcmp(pid_t pid1, pid_t pid2, int type, unsigned long idx1, unsigned long idx2) {
   return syscall(SYS_kcmp, pid1, pid2, type, 0, idx1, idx2);
 }
+#endif
 
 TEST(kernel_config, NOT_CONFIG_SYSVIPC) {
+#ifdef HAS_KCMP
   pid_t pid = getpid();
   int ret = kcmp(pid, pid, KCMP_SYSVSEM, 0, 0);
   int error = (ret == -1) ? (errno == ENOSYS) ? EOPNOTSUPP : errno : 0;
   EXPECT_EQ(-1, kcmp(pid, pid, KCMP_SYSVSEM, 0, 0));
   EXPECT_EQ(EOPNOTSUPP, error);
+#endif
   EXPECT_EQ(-1, access("/proc/sysvipc", F_OK));
+  EXPECT_EQ(-1, access("/proc/sysvipc/msg", F_OK));
+  EXPECT_EQ(-1, access("/proc/sysvipc/sem", F_OK));
+  EXPECT_EQ(-1, access("/proc/sysvipc/shm", F_OK));
 }
 
