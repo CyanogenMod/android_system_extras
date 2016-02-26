2eb7904ffb4c59e159fccdf47021f95756dc2f19
Mohamad Ayyash <mkayyash@google.com>
2016-02-26 13:58:44 -0800

    Introduce blk_alloc_to_base_fs utility
    Converts regular block mapping files into the new base_fs file format
    for generating incremental ext4 images
    
    Internal Design Doc: go/incremental-ext4
    BUG: 26839493
    Change-Id: If8dbb49718226771495c044eb1a8403e99a971fb
    Signed-off-by: Mohamad Ayyash <mkayyash@google.com>

--- ext4_utils/Android.mk
+++ ext4_utils/Android.mk
@@ -48,6 +48,13 @@ LOCAL_CFLAGS_darwin := -DHOST
 LOCAL_CFLAGS_linux := -DHOST
 include $(BUILD_HOST_EXECUTABLE)
 
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := blk_alloc_to_base_fs.c
+LOCAL_MODULE := blk_alloc_to_base_fs
+LOCAL_SHARED_LIBRARIES += libcutils
+LOCAL_CFLAGS_darwin := -DHOST
+LOCAL_CFLAGS_linux := -DHOST
+include $(BUILD_HOST_EXECUTABLE)
 
 #
 # -- All host/targets excluding windows
